dd167a69e04e0db368c9c18bf7886b37aa624a5d
Michael Fenwick <mickyfen17@aol.com>
2017-06-17 14:50:25 -0600

    add dynamic map center for new markers

--- src/containers/LeafletMap.js
+++ src/containers/LeafletMap.js
@@ -6,18 +6,32 @@ class LeafletMap extends Component {
   constructor(props) {
     super(props)
     this.state = {
-
+      center: [39.750809, -104.996810],
     };
   }
+
+  componentWillReceiveProps(nextProps) {
+    if(this.props.locations.length !== 0) {
+      const newLocation = nextProps.locations[nextProps.locations.length - 1]
+      this.alterMapCenter(newLocation)
+    }
+  }
+
+  alterMapCenter(newLocation) {
+    const { lat, lng } = newLocation;
+    this.setState({ center: [lat, lng] });
+  }
+
   render() {
     return (
       <div className="map-container">
         <Map
           className="map"
           zoomControl={false}
-          center={[39.750809, -104.996810]}
+          center={ this.state.center }
           zoom={4}
           maxBounds={[[85, 100], [-85, -280]]}
+          animate={true}
         >
           <TileLayer
             url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
