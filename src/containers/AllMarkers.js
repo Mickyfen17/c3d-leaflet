adb92f5b6d3d391df6fe9aa4636c6fee3bd7b98f
Michael Fenwick <mickyfen17@aol.com>
2017-06-17 17:27:29 -0600

    pass polygon state and functions via container

--- src/containers/AllMarkers.js
+++ src/containers/AllMarkers.js
@@ -1,14 +1,20 @@
 /*eslint-disable no-unused-vars*/
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as actions from '../actions/polygonActions';
+
 import AllMarkers from '../components/AllMarkers'
 
 const mapStateToProps = (state) => {
-  return { locations: state.Locations.data }
+  return {
+    locations: state.Locations.data,
+    polygon: state.Polygon.polygonArray
+  }
 }
 
 const mapDispatchToProps = (dispatch) => {
-  return {}
+  return bindActionCreators(actions, dispatch);
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(AllMarkers);
