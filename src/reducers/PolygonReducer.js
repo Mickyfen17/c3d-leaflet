765ab35687d9f556ffd6c34e8b6d2684cac9ea79
Michael Fenwick <mickyfen17@aol.com>
2017-06-17 17:21:34 -0600

    add reducers for polygon actions

---
+++ src/reducers/PolygonReducer.js
@@ -0,0 +1,25 @@
+const initialState = {
+  polygonArray: [],
+};
+
+const PolygonLinks = (state = initialState, action) => {
+  switch (action.type) {
+    case 'ADD_POLY_LINK':
+      return Object.assign({}, state, {
+        polygonArray: state.polygonArray.concat([action.data]),
+      });
+
+
+    case 'REMOVE_POLY_LINK' :
+      const filteredPolys = state.polygonArray.filter(latLng =>
+        JSON.stringify(action.data) !== JSON.stringify(latLng))
+        return Object.assign({}, state, {
+          polygonArray: filteredPolys,
+        });
+
+    default:
+      return state;
+  }
+};
+
+export default PolygonLinks;
