6f33a9a6482daf92542e2c8f66daeca6d544791e
Michael Fenwick <mickyfen17@aol.com>
2017-06-18 11:32:55 -0600

    remove app locals and add postgresql & sql

--- server/server.js
+++ server/server.js
@@ -3,46 +3,41 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
+const promise = require('bluebird');
+
+const options = {
+  promiseLib: promise,
+};
+const pgp = require('pg-promise')(options);
+const connectionString = 'postgres://localhost:5432/c3d';
+const db = pgp(connectionString);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-// Get all users
-const initialLocations = [
-  {
-    id: 'id1',
-    name: 'Denver',
-    lat: 39.742043,
-    lng: -104.991531,
-  },
-  {
-    id: 'id2',
-    name: 'LA',
-    lat: 34.052235,
-    lng: -118.243683,
-  },
-  {
-    id: 'id3',
-    name: 'Boston',
-    lat: 42.364506,
-    lng: -71.038887,
-  },
-];
+app.get('/api/locations', (request, response) => {
+  db.any('SELECT * FROM locations')
+  .then(locations => {
+    response.status(200).json(locations)
+  })
+  .catch(error => {
+    response.status(500).send({ error });
+  });
+});
 
-app.locals.idIndex = 3;
-app.locals.locations = initialLocations;
-
-app.get('/locations', (request, response) => response.send({ locations: app.locals.locations }));
-
-app.post('/locations/new', (request, response) => {
+app.post('/api/locations/new', (request, response) => {
   const validLocation = ['name', 'lat', 'lng'].every(value => request.body[value]);
 
   if(!validLocation) response.status(422).send({ error: 'Missing content from location form' });
 
-  const newLocation = Object.assign({}, { id: `id${app.locals.locations.length + 1}`}, request.body);
-  app.locals.locations = app.locals.locations.concat(newLocation);
-  response.send(newLocation);
-})
+  db.one('INSERT INTO locations(name, lat, lng)' + 'VALUES(${name}, ${lat}, ${lng}) RETURNING *', request.body)
+  .then(newLocation => {
+    response.status(200).json( newLocation )
+  })
+  .catch((error) => {
+    response.status(500).send({ error });
+  });
+});
 
 app.use(express.static(path.resolve(__dirname, '..', 'build')));
 
